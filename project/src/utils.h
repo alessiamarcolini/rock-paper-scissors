148e91f8494c780f8d01061a9c5b6554b25770c3
alessiamarcolini <98marcolini@gmail.com>
2020-01-17 00:33:43 +0100

    Add RockPaperScissor enum

--- project/src/utils.h
+++ project/src/utils.h
@@ -7,4 +7,14 @@
 #define MAXLEN 7000 // max len of message
 
 #define MAX_TEAMS 32U
+
+enum rockPaperScissor
+{
+    rock,
+    paper,
+    scissor
+};
+
+typedef enum rockPaperScissor outcome;
+
 int matching(int numSquadre, int championship[MAXN][MAXN]);

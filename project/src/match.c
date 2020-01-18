714d28da7ce5fae2cf8760ab70975c52d3f5556f
alessiamarcolini <98marcolini@gmail.com>
2020-01-18 17:53:23 +0100

    10 matches with random outcomes

--- project/src/match.c
+++ project/src/match.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "utils.h"
 
 int main(int argc, char *argv[])
 {
 
-    printf("sono il match n %s", argv[1]);
-    printf("first player: %s - ", argv[2]);
-    printf("second player: %s - ", argv[3]);
+    int i, j;
+    int nTrials = 10;
 
+    char *matchId = argv[1];
+    char *firstPlayerId = argv[2];
+    char *secondPlayerId = argv[3];
+
+    printf("sono il match n %s", matchId);
+    printf("first player: %s - ", firstPlayerId);
+    printf("second player: %s", secondPlayerId);
+
+    outcome first;
+    outcome second;
+
+    outcome firstPlayerOutcomes[nTrials];
+    outcome secondPlayerOutcomes[nTrials];
+
+    int firstPlayerPointsMatches[nTrials];
+    int secondPlayerPointsMatches[nTrials];
+
+    /* Intializes random number generator */
+    srand(time(0));
+
+    for (i = 0; i < nTrials; i++)
+    {
+        printf("lancio n: %d ", i);
+
+        printf("%d -- ", (outcome)rand() % 3);
+
+        first = (outcome)rand() % 3;
+        second = (outcome)rand() % 3;
+        firstPlayerOutcomes[i] = first;
+        secondPlayerOutcomes[i] = second;
+
+        //printf("ok fatto ");
+    }
     return 0;
 }

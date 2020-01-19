c52fadf5acf165857a557f1ad041089119579689
leonardo_berrighi <leoberri243@gmail.com>
2020-01-19 01:54:31 +0100

    fix return

--- project/src/main.c
+++ project/src/main.c
@@ -16,7 +16,7 @@ char message[MAXLEN];
 
 char *buffer; // used to convert int to str
 
-int i, j;
+int i, j, k;
 
 int main(int argc, char *argv[])
 {
@@ -40,57 +40,58 @@ int main(int argc, char *argv[])
     pid_t pid;
     int status; // return status of child
 
-    for (i = 0; i < (playersNumber - 1)*2; i++) // for each day, (playersNumber - 1)*2 forward and return
-    {
-        if (i != 0)
+    for (k = 0; k < 2; k++) { //forward and return (fix rewrite on same buffer and re-do of same matrix)
+        for (i = 0; i < playersNumber - 1; i++) // for each day
         {
-            waitpid(pid, &status, 0);
-        }
-        pipe(fd); /* Create unnamed pipe */
-        //printf("\n\nSto forkando\n");
-
-        pid = fork();
-
-        if (pid > 0)
-        {                     // parent
-            close(fd[WRITE]); /* close other side */
-            bytesRead = read(fd[READ], message, MAXLEN);
-            fprintf(stderr, "- main: Read %d bytes: \n%s", bytesRead, message);
-            close(fd[READ]); /* close this side */
-        }
-        else
-        { // child
-            //printf("%d\n", fd);
-            dup2(fd[WRITE], WRITE);
-            close(fd[READ]);
-            close(fd[WRITE]);
-            //char *const paramList[] = {"bin/day", playersNumber_str, NULL};
-
-            char *paramList[playersNumber + 3];
-
-            paramList[0] = "bin/day";
-            paramList[1] = playersNumber_str;
-            paramList[playersNumber + 2] = NULL;
-
-            //fork
-
-            //char dayMatches[playersNumber*2];
-            for (j = 0; j < playersNumber; j++)
-            { // single day
-                snprintf(buffer, 1024, "%d", championship[i][j]);
-
-                paramList[j + 2] = malloc(sizeof(char) * 1024);
-                // TODO: fix memory leak
-                strcpy(paramList[j + 2], buffer);
-                //fprintf(stderr, "-----%s-----", buffer);
-                //paramList[j + 2] = buffer;
-                //buffer[0] = '\0';
+            if (i != 0)
+            {
+                waitpid(pid, &status, 0);
             }
+            pipe(fd); /* Create unnamed pipe */
+            //printf("\n\nSto forkando\n");
 
-            int e = execv(paramList[0], paramList);
-            // wait
+            pid = fork();
+
+            if (pid > 0)
+            {                     // parent
+                close(fd[WRITE]); /* close other side */
+                bytesRead = read(fd[READ], message, MAXLEN);
+                fprintf(stderr, "- main: Read %d bytes: \n%s", bytesRead, message);
+                close(fd[READ]); /* close this side */
+            }
+            else
+            { // child
+                //printf("%d\n", fd);
+                dup2(fd[WRITE], WRITE);
+                close(fd[READ]);
+                close(fd[WRITE]);
+                //char *const paramList[] = {"bin/day", playersNumber_str, NULL};
+
+                char *paramList[playersNumber + 3];
+
+                paramList[0] = "bin/day";
+                paramList[1] = playersNumber_str;
+                paramList[playersNumber + 2] = NULL;
+
+                //fork
+
+                //char dayMatches[playersNumber*2];
+                for (j = 0; j < playersNumber; j++)
+                { // single day
+                    snprintf(buffer, 1024, "%d", championship[i][j]);
+
+                    paramList[j + 2] = malloc(sizeof(char) * 1024);
+                    // TODO: fix memory leak
+                    strcpy(paramList[j + 2], buffer);
+                    //fprintf(stderr, "-----%s-----", buffer);
+                    //paramList[j + 2] = buffer;
+                    //buffer[0] = '\0';
+                }
+
+                int e = execv(paramList[0], paramList);
+                // wait
+            }
         }
     }
-
     return 0;
 }
